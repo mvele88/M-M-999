dad866f1733c0e9f4a856b3b9cdf1a19af12dc9c
mvele88 <lordknowledgebornallah@gmail.com>
2025-06-12 14:45:10 -0500

    Delete status.js

--- status.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/status', (req, res) => {
-  res.json({ status: 'ok', timestamp: new Date().toISOString() });
-});
-
-module.exports = app;
